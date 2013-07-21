9181380c5d7e25e524090c824309aebbe8143355
Christian Engel <christian.engel@wearekiss.com>
2013-07-21 23:05:44 +0200

    Added query logging to the MySQLi Class

--- Kiss/MySQLi.php
+++ Kiss/MySQLi.php
@@ -2,13 +2,12 @@
 /**
  * Extends the MySQLi class with useful functions.
  * @author Christian Engel <hello@wearekiss.com>
- * @version 1.5
+ * @version 1.6
  */
 
 namespace Kiss;
 
-class MySQLi extends mysqli
-{
+class MySQLi extends \mysqli {
     var $connected = false;
 
     var $last_query = '';
@@ -23,31 +22,55 @@ class MySQLi extends mysqli
      */
     public $prefix = 'tbl_';
 
+    private $logging = FALSE;
+
     /**
      * Constructor
      *
-     * @param string $host Hostname of the MySQL server
-     * @param string $user Username of the  MySQL server
-     * @param string $password Password of the  MySQL server
-     * @param string $database Name of the database to work with
-     * @param string $prefix (optional) Prefix for table names. Default = 'tbl_'
+     * @param {String} $host Hostname of the MySQL server, or array containing all parameters.
+     * @param {String} $user Username of the  MySQL server
+     * @param {String} $password Password of the  MySQL server
+     * @param {String} $database Name of the database to work with
+     * @param {String} [$prefix] Prefix for table names. Default = 'tbl_'
+     * @param {String} [$logging] Set to a filename to log all mySQL queries & errors to it.
      */
-    public function __construct($host, $user=NULL, $password=NULL, $database=NULL, $prefix = '')
-    {
-        if(is_array($host)){
+    public function __construct($host, $user = NULL, $password = NULL, $database = NULL, $prefix = '', $logging = NULL) {
+        if (is_array($host)) {
             $user = $host['user'];
             $password = $host['password'];
             $database = $host['database'];
             $prefix = (isset($host['prefix'])) ? $host['prefix'] : '';
+            if (isset($host['logging'])) {
+                $this->logging = $host['logging'];
+            }
             $host = $host['host'];
         }
 
+        if ($logging) {
+            $this->loggig = $logging;
+        }
+
+        if (is_string($this->logging)) {
+            $this->logging = fopen($this->logging, 'a');
+            fwrite($this->logging, 'REQUEST:' . "\n" . $_SERVER['REQUEST_METHOD'] . ' ' . $_SERVER['REQUEST_URI'] . "\n\n");
+        }
+
         parent::__construct($host, $user, $password, $database);
-        if ( ! $this->connect_error)
-        {
+        if (!$this->connect_error) {
             $this->connected = TRUE;
         }
         $this->prefix = $prefix;
+    }
+
+    private function logSQL($sql) {
+        if (!$this->logging) {
+            return;
+        }
+        fwrite($this->logging, 'QUERY:' . "\n" . $sql . "\n");
+        if ($this->error) {
+            fwrite($this->logging, 'ERROR:' . "\n" . $this->error . "\n");
+        }
+        fwrite($this->logging, "\n");
     }
 
     /**
@@ -55,10 +78,11 @@ class MySQLi extends mysqli
      * @param string $string
      * @return string
      */
-    public function escape($string)
-    {
-        if(get_magic_quotes_gpc()) $string = stripslashes($string);
-        return '\''.$this->real_escape_string($string).'\'';
+    public function escape($string) {
+        if (get_magic_quotes_gpc()) {
+            $string = stripslashes($string);
+        }
+        return '\'' . $this->real_escape_string($string) . '\'';
     }
 
     /**
@@ -66,8 +90,8 @@ class MySQLi extends mysqli
      * @param $string
      * @return void
      */
-    private function putPrefix($string){
-        return str_replace(' $$', ' '.$this->prefix, $string);
+    private function putPrefix($string) {
+        return str_replace(' $$', ' ' . $this->prefix, $string);
     }
 
     /**
@@ -77,21 +101,22 @@ class MySQLi extends mysqli
      * @param string|array $sqlQuery
      * @return array|mixed
      */
-    public function query($sqlQuery)
-    {
+    public function query($sqlQuery) {
         $sqlQuery = $this->putPrefix($sqlQuery);
-        if(is_array($sqlQuery)){
+        if (is_array($sqlQuery)) {
             $errors = array();
             $results = array();
-            foreach($sqlQuery as $sql){
+            foreach ($sqlQuery as $sql) {
                 $results[] = parent::query($sql);
                 $errors[] = $this->error;
+                $this->logSQL($sql);
             }
             $this->multi_error = $errors;
             return $results;
         }
         $this->last_query = $sqlQuery;
         $result = parent::query($sqlQuery);
+        $this->logSQL($sqlQuery);
         return $result;
     }
 
@@ -100,7 +125,7 @@ class MySQLi extends mysqli
      * @param $sqlQuery
      * @return int
      */
-    function queryAffect($sqlQuery){
+    function queryAffect($sqlQuery) {
         $this->query($sqlQuery);
         return $this->affected_rows;
     }
@@ -110,9 +135,11 @@ class MySQLi extends mysqli
      * @param string $sqlQuery
      * @return boolean
      */
-    function querySuccess($sqlQuery){
+    function querySuccess($sqlQuery) {
         $result = $this->query($sqlQuery);
-        if($result) return TRUE;
+        if ($result) {
+            return TRUE;
+        }
         return FALSE;
     }
 
@@ -121,11 +148,15 @@ class MySQLi extends mysqli
      * @param string $sqlQuery
      * @return integer|false
      */
-    public function queryInsert($sqlQuery)
-    {
+    public function queryInsert($sqlQuery) {
         $sqlQuery = $this->putPrefix($sqlQuery);
         $this->query($sqlQuery);
-        if(!$this->error) return $this->insert_id; else return false;
+        if (!$this->error) {
+            return $this->insert_id;
+        }
+        else {
+            return false;
+        }
     }
 
     /**
@@ -133,19 +164,18 @@ class MySQLi extends mysqli
      * @param string $sqlQuery
      * @return array|false
      */
-    public function queryAll($sqlQuery)
-    {
+    public function queryAll($sqlQuery) {
         $result = $this->query($sqlQuery);
-        if($result)
-        {
+        if ($result) {
             $ausgabe = array();
-            while($row = $result->fetch_assoc())
-            {
+            while ($row = $result->fetch_assoc()) {
                 $ausgabe[] = $row;
             }
             return $ausgabe;
         }
-        else return array();
+        else {
+            return array();
+        }
     }
 
     /**
@@ -153,14 +183,14 @@ class MySQLi extends mysqli
      * @param string $sqlQuery
      * @return array|false Associative Array
      */
-    public function queryRow($sqlQuery)
-    {
+    public function queryRow($sqlQuery) {
         $result = $this->query($sqlQuery);
-        if($result)
-        {
+        if ($result) {
             return $result->fetch_assoc();
         }
-        else return FALSE;
+        else {
+            return FALSE;
+        }
     }
 
     /**
@@ -168,15 +198,15 @@ class MySQLi extends mysqli
      * @param string $sqlQuery
      * @return mixed|false
      */
-    public function queryValue($sqlQuery)
-    {
+    public function queryValue($sqlQuery) {
         $result = $this->query($sqlQuery);
-        if($result)
-        {
+        if ($result) {
             $row = $result->fetch_array();
             return $row[0];
         }
-        else return FALSE;
+        else {
+            return FALSE;
+        }
     }
 
     /**
@@ -184,11 +214,11 @@ class MySQLi extends mysqli
      * @param string $sqlQuery
      * @return array|false
      */
-    public function queryAllValue($sqlQuery){
+    public function queryAllValue($sqlQuery) {
         $result = $this->query($sqlQuery);
-        if($result){
+        if ($result) {
             $out = array();
-            while($row = $result->fetch_array()){
+            while ($row = $result->fetch_array()) {
                 $out[] = $row[0];
             }
             return $out;
@@ -202,22 +232,19 @@ class MySQLi extends mysqli
      * @param array $array Associative Array. Keys will be mapped to database columns.
      * @return string
      */
-    function makeSqlSetString($array)
-    {
+    function makeSqlSetString($array) {
         $ausgabe = '';
 
-        if(is_array($array))
-        {
-            foreach($array as $key=>$wert)
-            {
-                if($ausgabe != '') $ausgabe .= ', ';
-                if(is_int($wert))
-                {
-                    $ausgabe .= '`'.$key.'`='.$wert;
+        if (is_array($array)) {
+            foreach ($array as $key => $wert) {
+                if ($ausgabe != '') {
+                    $ausgabe .= ', ';
                 }
-                else
-                {
-                    $ausgabe .= '`'.$key.'`='.$this->escape($wert);
+                if (is_int($wert)) {
+                    $ausgabe .= '`' . $key . '`=' . $wert;
+                }
+                else {
+                    $ausgabe .= '`' . $key . '`=' . $this->escape($wert);
                 }
             }
         }
@@ -232,51 +259,50 @@ class MySQLi extends mysqli
      * @param array $array Associative Array
      * @return string "([COLUMNS]) VALUES ([VALUES])"
      */
-    function makeSqlValueString($array)
-    {
+    function makeSqlValueString($array) {
         $ausgabe = '';
         $keys = array();
         $objects = array();
         $multi_mode = FALSE;
 
         //Fetch all keys.
-        foreach($array as $key => $value)
-        {
-            if(is_array($value))
-            {
+        foreach ($array as $key => $value) {
+            if (is_array($value)) {
                 $multi_mode = TRUE;
-                foreach($value as $k => $v){
-                    if(!in_array($k, $keys)) $keys[] = $k;
+                foreach ($value as $k => $v) {
+                    if (!in_array($k, $keys)) {
+                        $keys[] = $k;
+                    }
                 }
             }
-            else
-            {
+            else {
                 $multi_mode = FALSE;
                 $keys[] = $key;
             }
         }
 
         //Now collect (and interpolate if necessary) all objects
-        if($multi_mode){
-            foreach($array as $v){
+        if ($multi_mode) {
+            foreach ($array as $v) {
                 $obj = array();
-                foreach($keys as $k){
-                    $obj[] = (isset($v[$k])) ? (is_numeric($v[$k])) ? $v[$k]+0 : $this->escape($v[$k]) : $this->escape('');
+                foreach ($keys as $k) {
+                    $obj[] = (isset($v[$k])) ? (is_numeric($v[$k])) ? $v[$k] + 0 : $this->escape($v[$k]) : $this->escape('');
                 }
                 $objects[] = implode(', ', $obj);
             }
-        } else {
+        }
+        else {
             $obj = array();
-            foreach($keys as $k){
-                $obj[] = (is_numeric($array[$k])) ? $array[$k]+0 : $this->escape($array[$k]);
+            foreach ($keys as $k) {
+                $obj[] = (is_numeric($array[$k])) ? $array[$k] + 0 : $this->escape($array[$k]);
             }
             $objects[] = implode(', ', $obj);
         }
 
-        $ausgabe = '(`'.implode('`,`', $keys).
-                   '`) VALUES ('.implode('), (', $objects).')';
+        $ausgabe = '(`' . implode('`,`', $keys) .
+                '`) VALUES (' . implode('), (', $objects) . ')';
 
-        
+
         return $ausgabe;
     }
 
@@ -285,10 +311,11 @@ class MySQLi extends mysqli
      * @param string $tablename
      * @return void
      */
-    function truncate($tablename){
-        $sql = 'TRUNCATE TABLE '.$tablename;
+    function truncate($tablename) {
+        $sql = 'TRUNCATE TABLE ' . $tablename;
         $sql = $this->putPrefix($sql);
         $this->query($sql);
     }
 }
+
 ?>
